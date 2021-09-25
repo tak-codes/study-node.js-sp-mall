69bfac60dc43f1bd2f03cbb5a8d04f7e4b8aee84
DESKTOP-acer <takgue1224@gmail.com>
2021-09-25 14:30:56 +0900

    기본문법 if문
    나이로 맥주!

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var age = 20;
-console.log(age);
-
-var name = "John Doe"
-console.log(name);
-
-var pwersonArray = [10,20,30];
-console.log(pwersonArray[0]);
-
 var personDict = {"name": "Jogn Doe","age": 19};
-console.log(personDict['age'])
+
+if (personDict['age'] > 19) {
+    console.log('your beer!')
+} else {
+    console.log('Get out!!!')
+}
