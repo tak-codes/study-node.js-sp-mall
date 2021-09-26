1eb1e84fc4352d59deb09cc7345d9893d814aae0
DESKTOP-acer <takgue1224@gmail.com>
2021-09-26 23:29:37 +0900

    쇼핑몰을 위한 기본 페이지
    여기서 부터 쇼핑몰 만들기 시작하면 된다.

--- index.js
+++ index.js
@@ -29,27 +29,18 @@ app.use((req, res, next) => {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/test', (req, res) => {
   let name = req.query.name;
   res.render('test', {name});
 })
 
-app.get('/', (req, res) => {
-  res.send('<!DOCTYPE html>\
-  <html lang="en">\
-  <head>\
-      <meta charset="UTF-8">\
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">\
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-      <title>Document</title>\
-  </head>\
-  <body>\
-      Hi. I am with html<br>\
-      <a href="/hi">Say Hi!</a>\
-  </body>\
-  </html>')
+app.get('/home', (req, res) => {
+  res.render('index');
 })
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
+
+//여기까지가 홈화면 및 페이지 만들기 틀
