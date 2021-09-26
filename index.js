3e0280b6baae9e8f9faed1a71831ff6d8e94adf6
DESKTOP-acer <takgue1224@gmail.com>
2021-09-26 19:45:02 +0900

    static 추가
    index.js static 코드 추가 및 구름.jpg 사진 커밋

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const userRouter = require('./routes/user')
 
 //아래 2줄이 body를 편하게 쓰기위한 2가지 방법!
 //express.json() , express.urlencoded 
+//마지막 줄은 static 추가! 정적 파일을 제공하기 쉽게!
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(express.static('public'));
 
 app.use('/goods', goodsRouter)
 app.use('/user', userRouter)
