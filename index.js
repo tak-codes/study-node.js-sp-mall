eb424915558480f43bb809030cde0a89e20e79f8
DESKTOP-acer <takgue1224@gmail.com>
2021-09-26 19:40:20 +0900

    미들웨어와 데이터공을 위한 2가지 방법
    미들웨이 index.js에 추가!   express.json / express.urlencoded  추가!

--- index.js
+++ index.js
@@ -5,33 +5,27 @@ const port = 3000
 const goodsRouter = require('./routes/goods')
 const userRouter = require('./routes/user')
 
-app.use('/goods', goodsRouter)
+//아래 2줄이 body를 편하게 쓰기위한 2가지 방법!
+//express.json() , express.urlencoded 
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/goods', goodsRouter)
 app.use('/user', userRouter)
+
+
+//아래 두단락이 미들웨어!
+app.use((req, res, next) => {
+  console.log(req);
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  res.send('Welcome Home');
+});
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
 
-
-
-// 파일 나누기전에는 아래와 같이 일일이 라우터를 적어줘야 하지만 router 파일을 만들어
-// 분리를 해주면 좀더 간편하게 파일을 관리할수있습니다.
-// 변경점은 app.get -> router.get  및 next, 애로우 함수 제거 입니다.app
-
-
-// app.get('/goods/list', (req, res) => {
-//   res.send('상품 목록 페이지')
-// })
-
-// app.get('/goods/detail', (req, res) => {
-//   res.send('상품 상세 페이지')
-// })
-
-// app.get('/user/login', (req, res) => {
-//   res.send('로그인 페이지')
-// })
-
-// app.get('/user/register', (req, res) => {
-//   res.send('회원가입 페이지')
-// })
